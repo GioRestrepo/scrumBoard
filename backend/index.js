0da9e8c235184af33b6f36e5c1d0050701c323f5
GioRestrepo <giovannyestebanrestrepo@gmail.com>
2024-08-07 14:13:47 -0500

    update index file

--- backend/index.js
+++ backend/index.js
@@ -12,19 +12,19 @@ app.listen(process.env.PORT, () =>
   console.log("Port is running in port: ", process.env.PORT)
 );
 
-dbConnection(); */
-const express = require("express");
-const cors = require("cors");
-const {dbConnection} = require("./db/db");
-require("dotenv").config();
+dbConnection(); */ 
+const express = require( "express" );
+const cors = require( "cors" );
+const {dbConnection} = require( "./db/db" );
+require( "dotenv" ).config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors);
+app.use( express.json() );
+app.use( cors );
 
-app.listen(process.env.PORT, ()=>{
-   console.log("Backend server is running ok in port: ", process.env.PORT);
+app.listen( process.env.PORT, ()=>{
+   console.log( "Backend server is running ok in port: ", process.env.PORT );
 });
 
 dbConnection();
